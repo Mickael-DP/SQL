48c25ac05920ef3ded6056212e18efe77abf6672
Mickael-DP <m.dallepasqualine@gmail.com>
2020-11-03 11:45:22 +0100

    exercise finisehd

--- Niveau2/Exercice 3 - Vraie page login/login.php
+++ Niveau2/Exercice 3 - Vraie page login/login.php
@@ -11,7 +11,7 @@ if (isset($_POST ['submit'])){
     
     $tentative= date('Y-m-d H:i:s');
     
-    $success= $validLogin && $validPassword;
+    $success= $validLogin && $validPassword; 
 
     if ($success){
         
